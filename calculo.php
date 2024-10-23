0b7fd6e8c22cb8d57f22c5ee13a90929a0e9e093
wanvan10 <55166348+wanvan10@users.noreply.github.com>
2024-10-23 00:40:43 +0000

    Criação do projeto

---
+++ calculo.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: application/json');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $numero1 = $_POST['numero1'];
+    $numero2 = $_POST['numero2'];
+
+    $soma = $numero1 + $numero2;
+
+    echo json_encode(['soma' => $soma]);
+
+} else {
+    echo json_decode(['erro' => 'Método não suportado. Use o POST']);
+
+}
+
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Calculadora AJAX </h1>
+
+    <pre>
+        <label>Numero 01</label>
+        <input type="text" name="numero1" id="numero1"/>
+
+        <label>Numero 02</label>
+        <input type="text" name="numero2" id="numero2"/>
+
+        <button>Calcular com ajax</button>
+        <p id="resultado"></p>
+</pre>
+<script>
+    function calcular(){
+        const numero1 = document.getElementById("numero1").value;
+        const numero2 = document.getElementById("numero2").value;
+
+        fetch('/calculo.php', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringfy({
+                numero1: parseFloat(numero1),
+                numero2: parseFloat(numero2)
+            })
+        })
+        .then(resposta => resposta.json())
+        .then(dados =>
+        document.getElementById("resultado").innerHTML =
+            "Soma: " + dados.soma;
+    })
+    .catch(erro =>{
+        document.getElementById("resultado").innerHTML =
+        "Erro ao processar";
+    });
+    
+</body>
+</html>
